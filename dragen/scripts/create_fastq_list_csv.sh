68db7b6c5753c99b1fce30b0af1040628ac8e6a6
EWeatherill <ew7@sanger.ac.uk>
2018-01-24 18:24:31 +0000

    Cleaned up the variable expansions.

--- dragen/scripts/create_fastq_list_csv.sh
+++ dragen/scripts/create_fastq_list_csv.sh
@@ -30,34 +30,34 @@ fastq_directory=$2
 echo "RGID,RGSM,RGLB,Lane,Read1File,Read2File" >  $csv_file_name
 
 # For each entry in the command line specified directory:
-for val in $(ls ${fastq_directory}); do
+for val in $(ls "${fastq_directory}"); do
 
   if [ -d "${fastq_directory}/${val}" ]; then
     library_path="${fastq_directory}/${val}"
-    library=${val}
+    library="${val}"
 
-    for val in $(ls ${library_path}); do
+    for val in $(ls "${library_path}"); do
       read_group_path="${library_path}/${val}"
 
-      if [ -d ${read_group_path} ] && [ $(ls ${read_group_path}  | grep -E '.fastq' | wc -l) -gt 0 ]; then
-        RGID=$(basename $read_group_path)
+      if [ -d "${read_group_path}" ] && [ $(ls "${read_group_path}"  | grep -E '.fastq' | wc -l) -gt 0 ]; then
+        RGID=$(basename "${read_group_path}")
 
-        for entry in $(ls $read_group_path); do
+        for entry in $(ls "${read_group_path}"); do
 
-          if [[ $entry =~ '.fastq' ]]; then
+          if [[ "${entry}" =~ '.fastq' ]]; then
             fastq1="${read_group_path}/${entry}"
 
             # For Casava naming conventions used with the evaluation data.  The fields should be as follows:
             #     [0]      [1]    [2]    [3]             [4]
             # <SampleID>_<Index>_<Lane>_<Read>_<segment# and FileExt>
-            IFS='_' read -a fields <<< "$entry"
+            IFS='_' read -a fields <<< "${entry}"
 
             # Check for R2 file.  This will be appended to the row in the csv.
             r2_file_to_check="${fields[0]}_${fields[1]}_${fields[2]}_R2_${fields[4]}"
-            if [ $(ls $read_group_path | grep $r2_file_to_check) ]; then
+            if [ $(ls "${read_group_path}" | grep "${r2_file_to_check}") ]; then
 
               # Remember to include the dir for full path name.
-              fastq2="$read_group_path/$r2_file_to_check"
+              fastq2="${read_group_path}/${r2_file_to_check}"
             fi
 
             # Extract the sample name and lane number
@@ -71,7 +71,7 @@ for val in $(ls ${fastq_directory}); do
   
               # As per the header, the fields are as follows:
               # RGID, RGSM, RGLB, Lane, FASTQ1, [FASTQ2]     # With the fastq2 being optional but it must still be 'there'.
-              echo "${RGID},${RGSM},${library},${Lane},${fastq1},${fastq2}" >> $csv_file_name
+              echo "${RGID},${RGSM},${library},${Lane},${fastq1},${fastq2}" >> "${csv_file_name}"
             fi
           fi
         done 

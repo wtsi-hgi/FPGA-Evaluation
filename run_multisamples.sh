8ccfe931b3e89a98f5fdcd533b790054cb40f421
sm0179 <sm0179@well.ox.ac.uk>
2018-11-29 17:23:11 +0200

    scripts to run DeepVariant version 0.7.0 in GCP

--- run_multisamples.sh
+++ run_multisamples.sh
@@ -17,22 +17,32 @@ STAGING_FOLDER_NAME=stage
 OUTPUT_FILE_PREFIX="${4}"
 JOB_PREFIX="${5:-none}"
 
-#INPUT_BAM_FILES=( $(cat "${INPUT_BAM_LIST}") )
-
 for INPUT_BAM_FILE in $(cat "${INPUT_BAM_LIST}"); do
     echo "${INPUT_BAM_FILE}"
 
     IFS='/' read -a fields <<< "${INPUT_BAM_FILE}"
     SAMPLE="${fields[-2]}"
-    OUTPUT_DIR="${OUTPUT_BUCKET}/${SAMPLE}"
+    DEPTH="${fields[-4]}"
+    OUTPUT_DIR="${OUTPUT_BUCKET}/${DEPTH}/${SAMPLE}"
     
     if [[ "${JOB_PREFIX}" =~ 'none' ]]; then
-	JOB_PREFIX="${OUTPUT_FILE_PREFIX}_${SAMPLE}"
+	JOB_LABEL="${DEPTH//-/_}-${OUTPUT_FILE_PREFIX}-repl${SAMPLE: -1}"
+    else
+	JOB_LABEL="${JOB_PREFIX}-${DEPTH//-/_}-${OUTPUT_FILE_PREFIX}-repl${SAMPLE: -1}"
     fi
 
     FILE_DATE=$(date -u +"%Y%m%d")
-    OUTPUT_FILE_NAME="${OUTPUT_FILE_PREFIX}.${SAMPLE}.bam_to_vcf.${FILE_DATE}.vcf"
 
-    bash run_dv_pipeline.sh "${INPUT_REF_FILE}" "${INPUT_BAM_FILE}" "${OUTPUT_DIR}" "${OUTPUT_FILE_NAME}" "${JOB_PREFIX}"
+    if [[ "${INPUT_BAM_FILE}" == *.bam ]]; then
+	INPUT_BAI_FILE="${INPUT_BAM_FILE%%.bam}.bai"
+	OUTPUT_FILE_NAME="${DEPTH}.${OUTPUT_FILE_PREFIX}.${SAMPLE}.bam_to_vcf.${FILE_DATE}.vcf"
+    fi
+
+    if [[ "${INPUT_BAM_FILE}" == *.cram ]]; then
+	INPUT_BAI_FILE="${INPUT_BAM_FILE}.crai"
+	OUTPUT_FILE_NAME="${DEPTH}.${OUTPUT_FILE_PREFIX}.${SAMPLE}.cram_to_vcf.${FILE_DATE}.vcf"	
+    fi
+
+    bash run_dv_pipeline.sh "${INPUT_REF_FILE}" "${INPUT_BAM_FILE}" "${INPUT_BAI_FILE}" "${OUTPUT_DIR}" "${OUTPUT_FILE_NAME}" "${JOB_LABEL}"
 
 done
